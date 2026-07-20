4658555f1955f08c0d644856dec425834373ddf2
tuturax <arthurleq@gmail.com>
2026-07-20 23:58:34 +0200

    modif: agent.py

--- agent.py
+++ agent.py
@@ -13,14 +13,18 @@ class Agent:
         self.max_age = 100
 
         # energy will go down during the simulation, and if it reaches 0, the agent dies
-        self.energy = 100
+        self._energy = 100
+        self.energy_max = 150
         self.reproduction_energy_needed = 120
         self.reproduction_energy_cost = 50
 
+        # countdown is used to pause the agent when it is giving birth or eating
         self.countdown = 0
+        # countdown_cadavre is used to pause the agent when it is eated
+        self.countdown_cadavre = 0
 
         # direction (in radians)
-        self.direction = np.random.uniform(0, 2 * np.pi)
+        self._direction = np.random.uniform(0, 2 * np.pi)
 
         # After each update, the agent can change its direction by a maximum of max_angular_change
         self.max_angular_change = np.pi / 8
@@ -28,6 +32,32 @@ class Agent:
         # scalar speed
         self.speed = 1.
 
+    
+    @property
+    def energy(self):
+        return self._energy
+        
+    
+    @energy.setter
+    def energy(self, value):
+        self._energy = value
+        if self._energy < 0:
+            self._energy = 0
+        elif self._energy > self.energy_max:
+            self._energy = self.energy_max
+    
+    # getter of the direction of the agent
+    @property
+    def direction(self):
+        return self._direction
+
+    # setter of the direction of the agent
+    @direction.setter
+    def direction(self, value):
+        value = np.clip(value, 
+                        self._direction - self.max_angular_change, 
+                        self._direction + self.max_angular_change)
+        self._direction = value
     
     # method to change the direction of the agent by a proportion of the maximum angular change
     def change_direction(self, proportion):
@@ -41,32 +71,26 @@ class Agent:
         self.vx = self.speed * np.cos(self.direction)
         self.vy = self.speed * np.sin(self.direction)
 
-    def move(self, world):
-        # if the agent is in pause (giving birth or eat)
-        if self.countdown != 0 :
-            self.countdown -= 1
-            # speed = 0
-            self.x += 0
-            self.y += 0
-        # else, update of the velocity
-        else :
-            # toric world
-            x_limit = world.width
-            y_limit = world.height
-            
-            self.x += self.vx
-            self.y += self.vy
 
-            # if the agent goes out of the world, it appears on the other side
-            if self.x < 0:
-                self.x += x_limit
-            elif self.x > x_limit:
-                self.x -= x_limit
-            
-            if self.y < 0:
-                self.y += y_limit
-            elif self.y > y_limit:
-                self.y -= y_limit
+    def move(self, world):
+
+        # toric world
+        x_limit = world.width
+        y_limit = world.height
+        
+        self.x += self.vx
+        self.y += self.vy
+
+        # if the agent goes out of the world, it appears on the other side
+        if self.x < 0:
+            self.x += x_limit
+        elif self.x > x_limit:
+            self.x -= x_limit
+        
+        if self.y < 0:
+            self.y += y_limit
+        elif self.y > y_limit:
+            self.y -= y_limit
 
     def get_distance(self, other_agent: Agent, world: "World"):
         """
@@ -104,34 +128,53 @@ class Prey(Agent):
         super().__init__(x, y)
         self.speed = 1.0
 
+    @property
+    def energy(self):
+        if self._energy < 0:
+            self._energy = 0
+        elif self._energy > self.energy_max:
+            self._energy = self.energy_max
+        return self._energy
+    
+
     def runaway(self, world):
         """
         Detect predators in the world and change direction to run away from them.
         """
         distance_to_predators = np.inf
+        # default direction is the current direction of the prey
+        run_away_direction = self.direction
+
         for predator in world.predators:
             distance, direction = self.get_distance(predator, world)
             if distance < distance_to_predators:
                 distance_to_predators = distance
-                # Run away from the predator
+                # Run away from the predator ( the opposite direction => + pi)
                 run_away_direction = direction + np.pi
             
-        # We update the direction of the prey with a maximum angular change
-        diff_direction = self.direction - run_away_direction
-        diff_direction = np.clip(diff_direction, -self.max_angular_change, self.max_angular_change)
-        self.direction -= diff_direction
+        # We update the direction of the prey
+        # using the setter method of the direction property 
+        # to ensure the maximum angular change is respected
+        self.direction = run_away_direction
 
 
     def update(self, world):
         
         # update the direction
         self.runaway(world)
+        
         # update the velocity in x and y axis
         self.set_velocity()
+        
         # update the position of the prey in the world
-        self.move(world)
+        # if the agent is in pause (giving birth or eat)
+        if self.countdown != 0 :
+            self.countdown -= 1
+        else:
+            self.move(world)
 
-        # prey don't lose energy when moving, but they age
+        # prey gain energy over time (herbivore)
+        self.energy += 1
         self.age += 1
 
 #############################
@@ -146,6 +189,9 @@ class Predator(Agent):
         Detect prey in the world and change direction to hunt them.
         """
         distance_to_prey = np.inf
+        # default direction is the current direction of the predator
+        hunt_direction = self.direction
+
         for prey in world.preys:
             distance, direction = self.get_distance(prey, world)
             if distance < distance_to_prey:
@@ -153,18 +199,23 @@ class Predator(Agent):
                 # Move towards the prey
                 hunt_direction = direction
 
-        # We update the direction of the predator with a maximum angular change
-        diff_direction = hunt_direction - self.direction
-        diff_direction = np.clip(diff_direction, -self.max_angular_change, self.max_angular_change)
-        self.direction += diff_direction
+        # We update the direction of the predator
+        # using the setter method of the direction property 
+        # to ensure the maximum angular change is respected
+        self.direction = hunt_direction
 
     def update(self, world):
         # update the direction
         self.hunt(world)
         # update the velocity in x and y axis
         self.set_velocity()
+        
         # update the position of the predator in the world
-        self.move(world)
+        # if the agent is in pause (giving birth or eat)
+        if self.countdown != 0 :
+            self.countdown -= 1
+        else:
+            self.move(world)
 
         # predators lose energy when moving, and they age
         self.energy -= 2
--- world.py
+++ world.py
@@ -19,37 +19,50 @@ class World:
 
         self.color_prey = "blue"
         self.color_predator = "orange"
-    
+
+        self.list_predator_to_add = []
+        self.list_prey_to_add = []
+
     # method to add preys to the world
-    def add_prey(self, **karg):
+    def add_prey(self, **kwargs):
         """
         Add preys to the world. The preys can be added in two ways:
         number (int) : By specifying the number of preys to add. The preys will be added at random positions in the world.
         position (tuple) : By specifying the position of a single prey
         """
         # if an int is given, we create that many preys with random positions
-        for i in range(karg.get("number", 1)):
+        for i in range(kwargs.get("number", 1)):
             rand_pos = np.random.rand(2) * [self.width, self.height]
             self.preys.append(Prey(rand_pos[0], rand_pos[1]))
         # if a tuple is given, we create a prey at that position
-        if "position" in karg:
-            self.preys.append(Prey(karg["position"][0], karg["position"][1]))
+        if "position" in kwargs:
+            self.preys.append(Prey(kwargs["position"][0], kwargs["position"][1]))
     
     # method to add predators to the world
-    def add_predator(self, **karg):
+    def add_predator(self, **kwargs):
         """
         Add predators to the world. The predators can be added in two ways:
         1. By specifying the number of predators to add. The predators will be added at random positions in the world.
         2. By specifying the position of a single predator
         """
         # if an int is given, we create that many predators with random positions
-        for i in range(karg.get("number", 1)):
+        for i in range(kwargs.get("number", 1)):
             rand_pos = np.random.rand(2) * [self.width, self.height]
             self.predators.append(Predator(rand_pos[0], rand_pos[1]))
         # if a tuple is given, we create a predator at that position
-        if "position" in karg:
-            self.predators.append(Predator(karg["position"][0], karg["position"][1]))
+        if "position" in kwargs:
+            self.predators.append(Predator(kwargs["position"][0], kwargs["position"][1]))
 
+
+
+    def return_dict(self):
+        """
+        Return a dictionary containing the positions and direction of the preys and predators in the world.
+        """
+        return {
+            "preys": [(prey.x, prey.y, prey.direction) for prey in self.preys],
+            "predators": [(predator.x, predator.y, predator.direction) for predator in self.predators]
+        }
 
     # Method to eat a prey if a predator is close enough
     def eating(self):
@@ -82,8 +95,7 @@ class World:
                             predator.energy -= predator.reproduction_energy_cost
                             other_predator.energy -= other_predator.reproduction_energy_cost
                             # and a new predator is born
-                            self.add_predator(position=((predator.x + other_predator.x) / 2, (predator.y + other_predator.y) / 2))
-                            
+                            self.list_predator_to_add.append(((predator.x + other_predator.x) / 2, (predator.y + other_predator.y) / 2))
                             # countdown because they are exausted
                             # female
                             predator.countdown = 5
@@ -95,10 +107,10 @@ class World:
             for other_prey in self.preys:
                 if prey != other_prey:
                     distance = prey.get_distance(other_prey, self)[0]
-                    # if the preys are close enough
-                    if distance < 5.0: 
+                    # if the preys are close enough AND both preys have enough energy
+                    if distance < 5.0 and (prey.energy > prey.reproduction_energy_needed and other_prey.energy > other_prey.reproduction_energy_needed):
                             # new prey is born
-                            self.add_prey(position=((prey.x + other_prey.x) / 2, (prey.y + other_prey.y) / 2))
+                            self.list_prey_to_add.append(((prey.x + other_prey.x) / 2, (prey.y + other_prey.y) / 2))
 
                             # countdown because they are exausted
                             # female
@@ -134,5 +146,14 @@ class World:
 
         # remove dead agents
         self.remove_dead()
+
+        # add new agents to the world
+        for position in self.list_predator_to_add:
+            self.add_predator(position=position)
+        self.list_predator_to_add = []
+
+        for position in self.list_prey_to_add:
+            self.add_prey(position=position)
+        self.list_prey_to_add = []
 
         self.time += 1
